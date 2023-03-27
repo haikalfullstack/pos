1879d1dcb55246cb4d6247195c75051d35978b3f
ekalrozhan <ekalrozhan@gmail.com>
2023-03-28 04:24:20 +0800

    Cleaning template

--- resources/views/body/sidebar.blade.php
+++ resources/views/body/sidebar.blade.php
@@ -45,47 +45,16 @@
 
         <ul id="side-menu">
 
-            <li class="menu-title">Navigation</li>
+            <li class="menu-title mt-1">Navigation</li>
 
             <li>
-                <a href="#sidebarDashboards" data-bs-toggle="collapse">
-                    <i class="mdi mdi-view-dashboard-outline"></i>
-                    <span class="badge bg-success rounded-pill float-end">4</span>
-                    <span> Dashboards </span>
-                </a>
-                <div class="collapse" id="sidebarDashboards">
-                    <ul class="nav-second-level">
-                        <li>
-                            <a href="index.html">Dashboard 1</a>
-                        </li>
-                        <li>
-                            <a href="dashboard-2.html">Dashboard 2</a>
-                        </li>
-                        <li>
-                            <a href="dashboard-3.html">Dashboard 3</a>
-                        </li>
-                        <li>
-                            <a href="dashboard-4.html">Dashboard 4</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-
-            <li class="menu-title mt-2">Apps</li>
-
-            <li>
-                <a href="apps-calendar.html">
-                    <i class="mdi mdi-calendar"></i>
-                    <span> Calendar </span>
+                <a href="{{ route('dashboard') }}">
+                <i class="mdi mdi-view-dashboard-outline"></i>
+                    <span> Dashboard </span>
                 </a>
             </li>
 
-            <li>
-                <a href="apps-chat.html">
-                    <i class="mdi mdi-forum-outline"></i>
-                    <span> Chat </span>
-                </a>
-            </li>
+         
 
             <li>
                 <a href="#sidebarEcommerce" data-bs-toggle="collapse">
